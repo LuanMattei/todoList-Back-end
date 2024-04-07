46035caa64461336ce5d793e944dc62f6a1ff1f6
luan mattei <luan.s.mattei@gmail.com>
2024-04-06 22:31:31 -0300

    criação dos cards
    Signed-off-by: luan mattei <luan.s.mattei@gmail.com>

--- todolist_back/core/models.py
+++ todolist_back/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    status = models.IntegerField()
+    
+    def __str__(self):
+        return super().__str__()
--- todolist_back/core/views.py
+++ todolist_back/core/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Card
 
 # Create your views here.
+
+def get(request):
+    card = Card.objects.all()
+    data = {
+        'card': [
+            {'id': Card.id, 'nome': Card.nome}
+            for card in card
+        ]
+    }
+    return JsonResponse(data)
+
+def save(request):
+    if request.method == 'POST':
+        vnome = request.POST.get("nome")
+        vdescricao = request.POST.get("descricao")
+        vstatus = request.POST.get("status")
+        novo_card = Card.objects.create(nome=vnome, descricao=vdescricao, status=vstatus)
+        cards = Card.objects.all()
+        data = {
+            'novo_card': {
+                'nome': novo_card.nome,
+                'descricao': novo_card.descricao,
+                'status': novo_card.status
+            },
+            'todos_cards': [
+                {'nome': card.nome, 'descricao': card.descricao, 'status': card.status} 
+                for card in cards
+            ]
+        }
+        return JsonResponse(data)
